570921d3626af94f3d0e38df328d3b25c1b97a59
0xENDER <72353925+0xENDER@users.noreply.github.com>
2026-03-15 00:21:59 +0200

    add shared cmake files

---
+++ .cmake/shared.cmake
@@ -0,0 +1,14 @@
+# CMake functions & macros
+include(${JUG_CMAKE_DIR}/utility.cmake)
+
+# Manage C++ compiler settings
+include(${JUG_CMAKE_DIR}/compiler/cpp.cmake)
+
+# Manage binary output
+include(${JUG_CMAKE_DIR}/build-output.cmake)
+
+# Manage shared dependencies
+include(${JUG_CMAKE_DIR}/dependencies.cmake)
+
+# CMake files that are not related to the main building process
+include(${JUG_CMAKE_DIR}/other/legal.cmake)
