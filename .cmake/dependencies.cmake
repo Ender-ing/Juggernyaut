2809f7c0d6a42953e85a6118a9d90194ff97ed2a
0xENDER <72353925+0xENDER@users.noreply.github.com>
2026-04-08 05:53:49 +0300

    Update dependencies.cmake

--- .cmake/dependencies.cmake
+++ .cmake/dependencies.cmake
@@ -149,12 +149,21 @@ set(ANTLR_EXECUTABLE ${JUG_DEPENDENCIES_ANTLR4_JAR_PATH})
 if(NOT CMAKE_BUILD_TYPE STREQUAL "Debug")
     if(NOT TARGET mimalloc)
         set(MIMALLOC_VERSION 3.2.8 CACHE STRING "mimalloc version" FORCE)
-        FetchContent_Declare(
-            mimalloc
-            GIT_REPOSITORY https://github.com/microsoft/mimalloc.git
-            GIT_TAG v${MIMALLOC_VERSION}
-            SOURCE_DIR ${JUG_DEPENDENCIES_DIR}/mimalloc
-        )
+        set(JUG_DEP_MIMALLOC_LIB_PATH ${JUG_DEPENDENCIES_DIR}/mimalloc)
+        if(EXISTS ${JUG_DEP_MIMALLOC_LIB_PATH}/CMakeLists.txt)
+            FetchContent_Declare(
+                mimalloc
+                GIT_TAG v${MIMALLOC_VERSION}
+                SOURCE_DIR ${JUG_DEP_MIMALLOC_LIB_PATH}
+            )
+        else()
+            FetchContent_Declare(
+                mimalloc
+                GIT_REPOSITORY https://github.com/microsoft/mimalloc.git
+                GIT_TAG v${MIMALLOC_VERSION}
+                SOURCE_DIR ${JUG_DEP_MIMALLOC_LIB_PATH}
+            )
+        endif()
         set(MI_BUILD_STATIC OFF CACHE BOOL "Build static library" FORCE)
         set(MI_BUILD_SHARED ON CACHE BOOL "Build shared library" FORCE)
         set(MI_BUILD_OBJECT OFF CACHE BOOL "Build object library" FORCE)
