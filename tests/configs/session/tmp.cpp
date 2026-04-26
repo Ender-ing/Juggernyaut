0504dab14f466af0137d72d4855788c0b7e42968
0xENDER <72353925+0xENDER@users.noreply.github.com>
2026-04-27 01:43:47 +0300

    Add configs dummy tests

---
+++ tests/configs/session/tmp.cpp
@@ -0,0 +1,14 @@
+/**
+ * @brief
+ * Parser tests
+**/
+
+#include "../../gtest.hpp"
+
+TEST(ConfigsSession, Temporary1) {
+    EXPECT_EQ(1, 1);
+}
+
+TEST(ConfigsSession, Temporary2) {
+    EXPECT_NE(1, 0);
+}
